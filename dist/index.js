f93db7b96d5a5553abc70f86eefc5a10d73b2180
Birat Poudel <poudel.birat25@gmail.com>
2023-06-08 13:37:51 +0545

    Fundamentals of TypeScript

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Hello World");
+//Fundamental Types
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+level = 1;
+level = "a";
+let numbers = [1, 2, 3];
+let sum = 0;
+numbers.forEach(n => sum += n);
+console.log(sum);

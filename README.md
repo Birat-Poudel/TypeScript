bdf5984881de30964f8f9fb16e0940889027fa08
Birat Poudel <poudel.birat25@gmail.com>
2023-06-08 13:39:59 +0545

    Fundamentals of TypeScript

--- README.md
+++ README.md
@@ -26,7 +26,38 @@ tsc --init
 ### Modifying tsconfig.json file
 
 ```
-"target": "es2016"
-"rootDir": "./src"
-"outDir": "./dist"
+"target": "es2016",
+"rootDir": "./src",
+"outDir": "./dist",
+"noImplicitAny": true,
+"noUnusedParameters": true,
+"noImplicitReturns": true,
+"noUnusedLocals": true
 ```
+
+## Fundamental Types
+
+JavaScript Data Types
+
+1. number : 1,2,3
+2. string : 'hi', 'hello', '123'
+3. boolean : true, false
+4. null : Intentional absence of value
+5. undefined : Absence of value
+6. object : const person = {firstName:"John", lastName:"Doe"}
+7. symbol : Immutable and unique
+
+```
+Symbol
+
+const x = Symbol('hey');
+console.log(x.description); // hey
+```
+
+TypeScript Extends the list by adding :
+
+1. any
+2. unknown
+3. never
+4. enum
+5. tuple
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("Hello World")
+//Fundamental Types
+
+let sales: number = 123_456_789
+let course: string = "TypeScript"
+let is_published: boolean = true
+
+let level: any
+level = 1
+level = "a"
+let g: any = []
+
+let numbers: number[] = [1,2,3] //Array
+let sum: number = 0
+numbers.forEach(n => sum += n) //Calls a function for each element in an array
+console.log(sum)
+
+let user: [number, string] = [1, 'Birat'] //Tuple
+
+const enum Size {Small = 1, Medium = 2, Large = 3} //Enum
+let mySize: Size = Size.Medium
+
+function calculateTax(income: number, taxYear = 2022): number{ //Function
+    if (taxYear < 2022)
+        return income*1.2
+    return income*1.3
+}
+calculateTax(200_000)
+
+let employee: { //Object
+    readonly id: number, 
+    name?: string //To make a property optional
+    retire: (date: Date) => void
+} = {
+    id: 1,
+    name: "Birat",
+    retire: (date: Date) => console.log(date)
+}
